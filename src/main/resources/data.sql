888eec19a882371872078e5abed2addbbf8bb1bf
Naoto Hori <naoto.hori@broadleaf.co.jp>
2022-07-15 10:24:21 +0900

    springboot rest api

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,14 @@
-INSERT INTO inquiry(name, email, contents, created)
-VALUES('Ethan', 'sample@example.com', 'Hello', '2019-11-12 08:34:19');
-INSERT INTO inquiry(name, email, contents, created)
-VALUES('Emma', 'sample2@example.com', 'GoodMorning', '2019-12-18 12:10:52');
-INSERT INTO inquiry(name, email, contents, created)
-VALUES('William', 'sample3@example.com', 'GoodEvening', '2019-12-18 15:10:52');
+INSERT INTO tutorials(description, published, title)
+VALUES('Description for Tutorial #1', 0, 'Spring Boot Tutorial #1');
 
-INSERT INTO survey(age, satisfaction, comment, created)
-VALUES(23, 5, 'Good!', '2019-01-03 11:02:35');
-INSERT INTO survey(age, satisfaction, comment, created)
-VALUES(47, 4, 'Excellent', '2019-02-18 22:35:54');
-INSERT INTO survey(age, satisfaction, comment, created)
-VALUES(47, 4, 'Excellent', '2019-02-18 22:35:54');
+INSERT INTO tutorials(description, published, title)
+VALUES('Description for Tutorial #2', 0, 'Spring Boot Tutorial #2');
+
+INSERT INTO tutorials(description, published, title)
+VALUES('Description for Tutorial #3', 0, 'Spring Boot Tutorial #3');
+
+INSERT INTO tutorials(description, published, title)
+VALUES('Description for Tutorial #4', 0, 'Spring Boot Tutorial #4');
+
+INSERT INTO tutorials(description, published, title)
+VALUES('Description for Tutorial #5', 0, 'Spring Boot Tutorial #5');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,22 +1,10 @@
-DROP TABLE IF EXISTS inquiry;
-DROP TABLE IF EXISTS survey;
+DROP TABLE IF EXISTS tutorials;
 
-CREATE TABLE inquiry
+CREATE TABLE tutorials
 (
    id INT NOT NULL AUTO_INCREMENT,
-   name VARCHAR(100) NOT NULL,
-   email VARCHAR(100) NOT NULL,
-   contents VARCHAR(500) NOT NULL,
-   created DATETIME NOT NULL,
-   PRIMARY KEY(id)
-);
-
-CREATE TABLE survey
-(
-   id INT NOT NULL AUTO_INCREMENT,
-   age INT NOT NULL,
-   satisfaction INT NOT NULL,
-   comment VARCHAR(100),
-   created DATETIME NOT NULL,
+   description VARCHAR(255) NOT NULL,
+   published bit(1) NOT NULL,
+   title VARCHAR(255) NOT NULL,
    PRIMARY KEY(id)
 );
